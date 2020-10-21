7f54563d86bc2e9c4e719606327b04ce8814fae0
hahadu <mhyzhu@163.com>
2020-10-22 00:21:08 +0800

    update

--- src/CurlClient.php
+++ src/CurlClient.php
@@ -14,39 +14,18 @@
 namespace Hahadu\HttpClass;
 class CurlClient{
     /**
-     *http curl post提交数据
-     *@param string $url
-     *@param array $post_data
+     * 判断是否SSL协议
+     * @return boolean
      */
-    static public function post($url = '', $post_data = array()) {
-        if (empty($url) || empty($post_data)) {
-            return false;
+    static public function is_ssl()
+    {
+        if (isset($_SERVER['HTTPS']) && ('1' == $_SERVER['HTTPS'] || 'on' == strtolower($_SERVER['HTTPS']))) {
+            return true;
+        } elseif (isset($_SERVER['SERVER_PORT']) && ('443' == $_SERVER['SERVER_PORT'])) {
+            return true;
         }
-        if(is_array($post_data)){
-            $post_data = http_build_query($post_data);
-        }
-
-        $ch = curl_init();//初始化curl
-        if(stripos($url, 'https://') !== FALSE) {
-            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
-            curl_setopt($ch, CURLOPT_SSLVERSION, 1);
-        }
-        curl_setopt($ch, CURLOPT_URL,$url);//抓取指定网页
-        curl_setopt($ch, CURLOPT_HEADER, 0);//设置header
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);//要求结果为字符串且输出到屏幕上
-        curl_setopt($ch, CURLOPT_POST, TRUE);//post提交方式
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
-        $content = curl_exec($ch);//运行curl
-		$status = curl_getinfo($ch);
-        curl_close($ch);
-        if (isset($status['http_code']) && $status['http_code'] == 200) {
-            return $content;
-        } else {
-            return FALSE;
-        }
-    }      
-
+        return false;
+    }
     /**
      * http curl get
      * @param string $url
@@ -68,7 +47,7 @@ class CurlClient{
         }
 
         $ch = curl_init();
-        if(stripos($url, 'https://') !== FALSE) {
+        if(self::is_ssl()) {
             curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
             curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
             curl_setopt($ch, CURLOPT_SSLVERSION, 1);
@@ -76,6 +55,38 @@ class CurlClient{
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1 );
         $content = curl_exec($ch);
+        $status = curl_getinfo($ch);
+        curl_close($ch);
+        if (isset($status['http_code']) && $status['http_code'] == 200) {
+            return $content;
+        } else {
+            return FALSE;
+        }
+    }
+    /**
+     *http curl post提交数据
+     *@param string $url
+     *@param array $post_data
+     */
+    static public function post($url = '', $post_data = array()) {
+        if (empty($url) || empty($post_data)) {
+            return false;
+        }
+        if(is_array($post_data)){
+            $post_data = http_build_query($post_data);
+        }
+        $ch = curl_init();//初始化curl
+        if(self::is_ssl()) {
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+            curl_setopt($ch, CURLOPT_SSLVERSION, 1);
+        }
+        curl_setopt($ch, CURLOPT_URL,$url);//抓取指定网页
+        curl_setopt($ch, CURLOPT_HEADER, 0);//设置header
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);//要求结果为字符串且输出到屏幕上
+        curl_setopt($ch, CURLOPT_POST, TRUE);//post提交方式
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
+        $content = curl_exec($ch);//运行curl
         $status = curl_getinfo($ch);
         curl_close($ch);
         if (isset($status['http_code']) && $status['http_code'] == 200) {
